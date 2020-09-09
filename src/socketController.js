341a06af5a9ec9f0ce6274588bcd9581229e47c4
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-09-09 21:05:22 +0900

    Notification and Chat

---
+++ src/socketController.js
@@ -0,0 +1,21 @@
+import events from "./events";
+
+const socketController = (socket) => {
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
+
+  socket.on(events.setNickname, ({ nickname }) => {
+    console.log(nickname);
+    socket.nickname = nickname;
+    broadcast(events.newUser, { nickname });
+  });
+
+  socket.on(events.disconnect, () => {
+    broadcast(events.disconnected, { nickname: socket.nickname });
+  });
+
+  socket.on(events.sendMsg, ({ message }) => {
+    broadcast(events.newMsg, { message, nickname: socket.nickname });
+  });
+};
+
+export default socketController;
