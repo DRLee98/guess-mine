72966b9837ce496e87e67c5aa7076c11863c6cfc
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-09-10 21:34:49 +0900

    paint add

--- src/events.js
+++ src/events.js
@@ -5,6 +5,13 @@ const events = {
   disconnected: "disconnected",
   sendMsg: "sendMsg",
   newMsg: "newMsg",
+  beginPath: "beginPath",
+  strokePath: "strokePath",
+  beganPath: "beganPath",
+  strokedPath: "strokedPath",
+  fill: "fill",
+  filled: "filled",
+  playerUpdate: "playerUpdate",
 };
 
 export default events;
--- src/socketController.js
+++ src/socketController.js
@@ -1,20 +1,40 @@
 import events from "./events";
 
-const socketController = (socket) => {
+let sockets = [];
+
+const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
+  const superBroadcast = (event, data) => io.emit(event, data);
+  const sendPlayerUpdate = () =>
+    superBroadcast(events.playerUpdate, { sockets });
 
   socket.on(events.setNickname, ({ nickname }) => {
-    console.log(nickname);
     socket.nickname = nickname;
+    sockets.push({ id: socket.id, points: 0, nickname: nickname });
     broadcast(events.newUser, { nickname });
+    sendPlayerUpdate();
   });
 
   socket.on(events.disconnect, () => {
+    sockets = sockets.filter((aSocket) => aSocket.id !== socket.id);
     broadcast(events.disconnected, { nickname: socket.nickname });
+    sendPlayerUpdate();
   });
 
-  socket.on(events.sendMsg, ({ message }) => {
-    broadcast(events.newMsg, { message, nickname: socket.nickname });
+  socket.on(events.sendMsg, ({ message }) =>
+    broadcast(events.newMsg, { message, nickname: socket.nickname })
+  );
+
+  socket.on(events.beginPath, ({ x, y }) =>
+    broadcast(events.beganPath, { x, y })
+  );
+
+  socket.on(events.strokePath, ({ x, y, color }) => {
+    broadcast(events.strokedPath, { x, y, color });
+  });
+
+  socket.on(events.fill, ({ color }) => {
+    broadcast(events.filled, { color });
   });
 };
 
